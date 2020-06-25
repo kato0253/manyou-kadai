cef5a9dd2f9238dd14ddc6fd24f27febd91fae9c
kato <user@MacBook-Air.local>
2020-06-25 23:35:26 +0900

    ERchart added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -95,4 +95,9 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by(:selenium_chrome)
+  end
+end
 end
