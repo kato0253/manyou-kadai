fabfe2b30892489d165761af199f2d8a31a6989b
kato <user@MacBook-Air.local>
2020-06-25 20:15:00 +0900

    hint-2 cleared

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   describe 'タスク一覧画面' do
-    context 'タスクを作成した場合' do
-      it '作成済みのタスクが表示される'
+    # テストコードを it '~' do end ブロックの中に記載する
+    it '作成済みのタスクが表示される' do
+      # テストで使用するためのタスクを作成
+      task = FactoryBot.create(:task, name: 'task',detail: 'task1')
+      # タスク一覧ページに遷移
+      visit tasks_path
+      # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
+      # have_contentされているか。（含まれているか。）ということをexpectする（確認・期待する）
+      expect(page).to have_content 'task'
+      # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
     end
   end
   describe 'タスク登録画面' do
