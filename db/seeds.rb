95e2ae615dacf3cd2df9d316805a3ac5fa5ae93c
kato <user@MacBook-Air.local>
2020-07-03 23:42:08 +0900

    seed_date added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,47 @@
+email = Faker::Internet.email
+User.create!(id: 1,
+             name: 'sample',
+             email: email,
+             password: '00000000',
+             admin: 'true',
+            )
+
 priority_array=["高","中","低"]
 status_array=["未着手","着手中","完了"]
-3.times do |n|
-    name = Faker::Games::Pokemon.name
-    detail = Faker::Games::Pokemon.name
-    due = Faker::Date.between(from: 2.days.ago,to: Date.today)
-    priority = priority_array[rand(3)]  #enum
-    status = status_array[rand(3)]
-    Task.create!(name: name,
-                 detail: detail,
-                 due: due,
-                 priority: priority,
-                 status: status,
-                 )
-end
 
-3.times do |n|
   name = Faker::Games::Pokemon.name
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password,
+  detail = Faker::Games::Pokemon.name
+  end_date = Faker::Date.between(from: 2.days.ago,to: Date.today)
+  priority = priority_array[rand(3)]
+  status = status_array[rand(3)]
+  Task.create!(name: name,
+               detail: detail,
+               end_date: end_date,
+               priority: priority,
+               status: status,
+               user_id: 1,
               )
-end
+
+email = Faker::Internet.email
+User.create!(id: 2,
+             name: 'sample',
+             email: email,
+             password: '00000000',
+             admin: 'false',
+            )
+
+priority_array=["高","中","低"]
+status_array=["未着手","着手中","完了"]
+
+  name = Faker::Games::Pokemon.name
+  detail = Faker::Games::Pokemon.name
+  end_date = Faker::Date.between(from: 2.days.ago,to: Date.today)
+  priority = priority_array[rand(3)]
+  status = status_array[rand(3)]
+  Task.create!(name: name,
+               detail: detail,
+               end_date: end_date,
+               priority: priority,
+               status: status,
+               user_id: 2,
+              )
