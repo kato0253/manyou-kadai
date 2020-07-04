60e845d1c889833d895ee7c1af210611757718d3
kato <user@MacBook-Air.local>
2020-07-04 21:21:25 +0900

    seed changed

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    id { 1 }
+    id { 12 }
     name { 'sample' }
     email { 'sample@example.com' }
     password { '00000000' }
     admin { false }
   end
   factory :admin_user, class: User do
-    id { 2 }
+    id { 13 }
     name { 'admin' }
     email { 'admin@example.com' }
     password { '11111111' }
